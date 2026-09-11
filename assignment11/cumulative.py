445ad88c1c8cf8dbccaa28ee4da4c91e1db0af5a
fgcharlton <fgcharlton1@gmail.com>
2026-09-11 08:34:47 -0400

    Updated tasks 2 and 3 based on AirHub feedback

--- assignment11/cumulative.py
+++ assignment11/cumulative.py
@@ -19,14 +19,10 @@ try:
 except Exception as e:
     print(f"Exception caught: {e}")
 
-def cumulative(row):
-   totals_above = df['total_price'][0:row.name+1]
-   return totals_above.sum()
-
 df['cumulative'] = df['total_price'].cumsum()
 
 # Create a line plot of cumulative revenue vs. order_id
-df.plot(x="order_id", y="cumulative", kind="line", title="Cumulative Revenue vs. Order_ID", legend = False)
+df.plot(x="order_id", y="cumulative", kind="line", title="cumulative revenue vs. order_id", legend = False)
 
 # Format line chart
 plt.xlabel('order_id')
@@ -38,6 +34,9 @@ plt.show()
 # Task 3: Interactive Visualizations with Plotly
 import plotly.express as px
 import plotly.data as pldata
+import webbrowser
+import os 
+
 df = pldata.wind(return_type='pandas')
 
 print("First 10 lines of the DataFrame")
@@ -49,7 +48,7 @@ print(df.tail(10))
 # Clean Data
 
 # Clean strength column
-df['strength'] = df['strength'].str.replace('[^0-9]', '', regex=True)
+df['strength'] = df['strength'].str.replace(r"[^\d.]", '', regex=True)
 
 # Convert strength column to a float
 df['strength'] = df['strength'].astype(float)
@@ -64,20 +63,13 @@ print("Confirm correct data types for the DataFrame")
 print(df.dtypes)
 
 # Create interactive scatter plot of strength vs. frequency, with colors based on direction
-import plotly.express as px
-import plotly.data as pldata
-
-df = pldata.wind(return_type='pandas') # Returns a DataFrame.  plotly.data has a number of sample datasets included.
 fig = px.scatter(df, x='strength', y='frequency', color='direction',
                  title="Wind Data, Strength vs. Frequency", hover_data=["frequency"])
 fig.write_html("wind.html", auto_open=True)
 
-# Open HTML to ensure it was created
-def read_html_file(file_path):
-    with open(file_path, 'r') as file:
-        html_content = file.read()
-    return html_content
-
-html_text = read_html_file('wind.html')
-
-print(html_text)
+# Ensure HTML was created
+if os.path.exists("wind.html") and os.path.getsize("wind.html") > 0:
+    print("wind.html was saved successfully.")
+    webbrowser.open("wind.html")
+else:
+    print("Error: wind.html was not saved correctly.")
